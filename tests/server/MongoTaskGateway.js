f86aa6387910b16396e3522b44335a0f38761585
Kristian Welsh <kriswelsh@gmail.com>
2019-05-28 13:15:15 +0100

    first version of DB tests finished

--- tests/server/MongoTaskGateway.js
+++ tests/server/MongoTaskGateway.js
@@ -6,49 +6,42 @@ var path = "../../src/server/storage/";
 var MongoTaskGateway = require(path + "MongoTaskGateway");
 var MongoConnection = require(path + "MongoConnection");
 
-function dropDatabase() {
-	MongoClient.connect("mongodb://localhost:27017")
-		.then(db => db.dropDatabase());
+// db gives documents uuids, set to 0 for comparison
+function assertEqual(expected, actual) {
+	expected[0]._id = 0;
+	actual[0]._id = 0;
+	expect(actual).to.deep.equal(expected);
 }
 
-/*******************************************
- * WARNING: these tests clear the database *
- *******************************************/
 describe('MongoTaskGateway', function(){
 	describe('#store(),retrieve()', function(){
 		it('should store and retrieve a task', function(done) {
-			this.timeout(4000);
+			this.timeout(6000);
+			// arrange
 			var connection = new MongoConnection("test");
 			var gateway = new MongoTaskGateway(connection);
 
-			var taskTask = {
+			var input = {
 				title: "title",
 				body: "body",
 				author: "author"
 			};
-			var expectedResult = {
-				_id: "0",
+			var expected = [{
 				title: "title",
 				body: "body",
 				author: "author"
-			};
+			}];
 
-			console.log("HEY1");
-			gateway.store(taskTask)
+			connection.drop()
+			// act
+			.then(() => gateway.store(input))
 			.then(() => gateway.retrieveAll())
-			.then(queryResult => {
-				queryResult[0]._id = 0;
-				console.log("HEY2");
-				console.log(queryResult);
-				expect(queryResult).to.deep.equal(expectedResult);
-			}).finally(() => {
-				connection.drop();
-				done();
-			});
-
-			//task._id = "0";
-			//retrieved[0]._id = "0";
-			//expect(retrieved).to.be.eql([task]);
+			// assert
+			.then(actual => assertEqual(expected, actual))
+			// teardown & end test
+			.catch(err => done(err))
+			.finally(() => connection.drop())
+			.then(() => done());
 		});
 	});
 });
