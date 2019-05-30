bc23ef2ce095463a2a308e50e54fd0557f3374ae
Kristian Welsh <kriswelsh@gmail.com>
2019-05-30 14:41:51 +0100

    Implement create task
    Allows users to insert tasks into the database, one of the main features
    
    Pipes user input from a post request into a json object which is sent to
    a CreateUser interactor.

--- src/display/express-frontend.js
+++ src/display/express-frontend.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const MongoConnection = require('../storage/MongoConnection');
+const MongoTaskGateway = require('../storage/MongoTaskGateway');
+const CreateTask = require('../task/CreateTask');
 
+// application setup
+const dbConnection = new MongoConnection("tasks");
+const mongoGateway = new MongoTaskGateway(dbConnection);
+const createTask = new CreateTask(mongoGateway);
+
+// frontend setup
 const app = express();
 const port = 3000;
 
@@ -14,6 +23,9 @@ app.engine('hbs', hbs(hbsOptions));
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'hbs');
 
+// middleware setup
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded to req.body
+
 // serve requests
 app.get('/', (req, res) => {
 	// in the future call applicaiton code based on req, then from
@@ -21,6 +33,24 @@ app.get('/', (req, res) => {
 	var viewData = {title: 'Index', message: 'Hello, world!'};
 	res.render('index', viewData);
 });
+app.post('/task/create', (req, res) => {
+	var requestModel = formToJson(req.body);
+	console.log(requestModel);
+	const responseModel = createTask.execute(requestModel);
+	// todo: view model & 
+	
+	var viewData = {title: 'Index', message: 'Hello, world!'};
+	res.render('index', viewData);
+});
+
+function formToJson(formBody) {
+	var json = {
+		title: formBody.title,
+		body: formBody.body,
+		author: formBody.author,
+	};
+	return json;
+}
 
 // start server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/task/CreateTask.js
+++ src/task/CreateTask.js
@@ -3,7 +3,9 @@ class CreateTask {
 		this.gateway = gateway;
 	}
 	execute(request) {
+		console.log("request recieved by CreateTask: " + request);
 		this.gateway.store(request);
+		// todo: returns status pending until store promise resolves, then update with successful
 		return {status: "successful"};
 	}
 }
