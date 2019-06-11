6ddd8d1f66a8d6113dc49debf511f92d975e674a
Kristian Welsh <kriswelsh@gmail.com>
2019-06-11 14:34:46 +0100

    Made MongoTaskGateway a true unit test
    Previously MongoTaskGateway test was an integration test that ran slowly
    becuase it hit the database. Testing MongoTaskGateway independant of
    MongoConnection will allow tests to be run more quickly.
    
    The new tests mock across the seam between MongoTaskGateway and
    MongoConnection, allowing tests to exercise MongoTaskGateway without
    hitting the database.

---
+++ tests/unit/MongoTaskGateway.js
@@ -0,0 +1,55 @@
+var chai = require("chai");
+var sinon = require("sinon");
+var expect = chai.expect;
+
+var path = "../src/storage/";
+var MongoTaskGateway = require(path + "MongoTaskGateway");
+
+var connection;
+var data;
+var findResults;
+
+function assert_connection_established_and_disbanded_correctly() {
+	chai.assert(connection.establish.calledOnce);
+	chai.assert(connection.disband.calledOnce);
+	chai.assert(connection.disband.calledAfter(connection.establish));
+}
+
+describe('MongoTaskGateway', function(){
+	beforeEach(() => {
+		connection = {
+			establish: sinon.stub(),
+			disband: sinon.spy(),
+		};
+		data = {
+			insertOne: sinon.stub(),
+			find: sinon.stub(),
+		};
+		findResults = {
+			toArray: () => findResults
+		};
+		connection.establish.returns(Promise.resolve(data));
+	});
+	describe('#store()', function(){
+		it('should store a task', async function() {
+			var gateway = new MongoTaskGateway(connection);
+			var task = {title: "title", body: "body", author: "author"};
+			
+			await gateway.store(task);
+			
+			sinon.assert.calledWith(data.insertOne, task);
+			assert_connection_established_and_disbanded_correctly();
+		});
+	});
+	describe('#retrieveAll()', function(){
+		it('should retrieve tasks', async function() {
+			data.find.returns(findResults);
+			var gateway = new MongoTaskGateway(connection);
+			
+			var result = await gateway.retrieveAll();
+			
+			chai.assert.equal(result, findResults);
+			assert_connection_established_and_disbanded_correctly();
+		});
+	});
+});
