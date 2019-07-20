1c818f0b97ae140b42402dea21f29106e1399fe7
Kristian Welsh <kriswelsh@gmail.com>
2019-07-20 16:21:21 +0100

    Refactor MongoTaskGateway tests
    - Changed uses of var to let or const as appropriate.
    - Single assert per test helps function composition with arrange act assert
    - pulled out a common arrange line into before function
    - blank lines between tests helps mental and physical navigation of file.

--- tests/unit/MongoTaskGateway.js
+++ tests/unit/MongoTaskGateway.js
@@ -1,15 +1,14 @@
-var chai = require("chai");
-var sinon = require("sinon");
-var expect = chai.expect;
+const chai = require("chai");
+const sinon = require("sinon");
+const expect = chai.expect;
 
-var path = "../../src/storage/";
-var MongoTaskGateway = require(path + "MongoTaskGateway");
+const path = "../../src/storage/";
+const MongoTaskGateway = require(path + "MongoTaskGateway");
 
-var connection;
-var data;
-var findResults;
-
-let idCount = 1;
+let connection;
+let data;
+let findResults;
+let gateway;
 
 function assert_connection_established_and_disbanded_correctly() {
 	chai.assert(connection.establish.calledOnce);
@@ -31,29 +30,41 @@ describe('MongoTaskGateway', function(){
 			toArray: () => findResults
 		};
 		connection.establish.returns(Promise.resolve(data));
-		data.insertOne.returns({ insertedId: "testId" + idCount++ });
+		data.insertOne.returns({ insertedId: "testId" });
+		gateway = new MongoTaskGateway(connection);
 	});
+
 	describe('#store()', function(){
-		it('should store a task', async function() {
-			var gateway = new MongoTaskGateway(connection);
-			var task = {title: "title", body: "body", author: "author"};
-			
-			let id = await gateway.store(task);
-			
-			sinon.assert.calledWith(data.insertOne, task);
-			chai.assert.equal("testId1", id);
+		it('connects and disconnects from the database cleanly', async function() {
+			let task = {title: "title", body: "body", author: "author"};
+			await gateway.store(task);
 			assert_connection_established_and_disbanded_correctly();
 		});
+
+		it('stores a task', async function() {
+			let task = {title: "title", body: "body", author: "author"};
+			await gateway.store(task);
+			sinon.assert.calledWith(data.insertOne, task);
+		});
+
+		it('returns the tasks id', async function() {
+			let task = {title: "title", body: "body", author: "author"};
+			let id = await gateway.store(task);
+			chai.assert.equal("testId", id);
+		});
 	});
+
 	describe('#retrieveAll()', function(){
-		it('should retrieve tasks', async function() {
+		it('connects and disconnects from the database cleanly', async function() {
 			data.find.returns(findResults);
-			var gateway = new MongoTaskGateway(connection);
-			
-			var result = await gateway.retrieveAll();
-			
-			chai.assert.equal(result, findResults);
+			await gateway.retrieveAll();
 			assert_connection_established_and_disbanded_correctly();
+		});
+
+		it('retrieves tasks', async function() {
+			data.find.returns(findResults);
+			let result = await gateway.retrieveAll();
+			chai.assert.equal(result, findResults);
 		});
 	});
 });
